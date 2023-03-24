8bc2f11dd8aa0038722c36a66dc4accf1f572791
dee <dljlixo@gmail.com>
2023-03-23 21:38:15 -0400

    testing5

--- config/keycodes.h
+++ config/keycodes.h
@@ -79,7 +79,6 @@
 
 
 #define ___ASTRK_    &kp KP_MULTIPLY        //DJ's testing *
-#define ____AT___    &kp AT                 //DJ's testing @
 #define KC_PERCENT   LS(NUMBER_5)            // %
 #define _PERCENT_    &kp KC_PERCENT          // %
 #define KC_SLASH     LS(NUMBER_7)            //  /
